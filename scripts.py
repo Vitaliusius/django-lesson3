ac89a573cbdfd15790da967872736a3c581baae6
Vitaliy <vitalianobro@gmail.com>
2026-02-14 10:52:22 +0300

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -8,23 +8,19 @@ from django.core.exceptions import ObjectDoesNotExist
 
 
 def fix_marks(childrens_name):
-    child = get_schoolkid_or_lessons(childrens_name)
+    child = get_schoolkid(childrens_name)
     Mark.objects.filter(schoolkid=child, points__lt=4).update(points=5)
 
 
 def delete_chastisements(childrens_name):
-    child = get_schoolkid_or_lessons(childrens_name)
+    child = get_schoolkid(childrens_name)
     Chastisement.objects.filter(schoolkid=child).delete()
 
 
-def get_schoolkid_or_lessons(childrens_name, subject=0):
+def get_schoolkid(childrens_name):
     try:
-        if subject==0:
-            schoolkid = Schoolkid.objects.get(full_name__contains=childrens_name)
-            return schoolkid
-        else:
-            lessons = Lesson.objects.filter(subject__title=subject, subject__year_of_study=6)
-            return lessons
+        schoolkid = Schoolkid.objects.get(full_name__contains=childrens_name)
+        return schoolkid
     except ObjectDoesNotExist:
         print('Неправильно введено имя')
     except Schoolkid.MultipleObjectsReturned:
@@ -32,45 +28,45 @@ def get_schoolkid_or_lessons(childrens_name, subject=0):
     
         
 def create_commendation(childrens_name, lessons_name):
-    lessons = get_schoolkid_or_lessons(childrens_name, lessons_name)
-    if lessons.exists()==True:
+    lessons = Lesson.objects.filter(subject__title=lessons_name, subject__year_of_study=6)
+    if lessons.exists():
         lesson = random.choice(lessons)
     else:
         print('Нет такого предмета')
         exit()
-    child = get_schoolkid_or_lessons(childrens_name)
+    child = get_schoolkid(childrens_name)
     praises = [
-                'Молодец!',
-                'Отлично!',
-                'Хорошо!',
-                'Гораздо лучше, чем я ожидал!',
-                'Ты меня приятно удивил!',
-                'Великолепно!',
-                'Прекрасно!',
-                'Ты меня очень обрадовал!',
-                'Именно этого я давно ждал от тебя!',
-                'Сказано здорово – просто и ясно!',
-                'Ты, как всегда, точен!',
-                'Очень хороший ответ!',
-                'Талантливо!',
-                'Ты сегодня прыгнул выше головы!',
-                'Я поражен!',
-                'Уже существенно лучше!',
-                'Потрясающе!',
-                'Замечательно!',
-                'Прекрасное начало!',
-                'Так держать!',
-                'Ты на верном пути!',
-                'Здорово!',
-                'Это как раз то, что нужно!',
-                'Я тобой горжусь!',
-                'С каждым разом у тебя получается всё лучше!',
-                'Мы с тобой не зря поработали!',
-                'Я вижу, как ты стараешься!',
-                'Ты растешь над собой!',
-                'Ты многое сделал, я это вижу!',
-                'Теперь у тебя точно все получится!'
+                    'Молодец!',
+                    'Отлично!',
+                    'Хорошо!',
+                    'Гораздо лучше, чем я ожидал!',
+                    'Ты меня приятно удивил!',
+                    'Великолепно!',
+                    'Прекрасно!',
+                    'Ты меня очень обрадовал!',
+                    'Именно этого я давно ждал от тебя!',
+                    'Сказано здорово – просто и ясно!',
+                    'Ты, как всегда, точен!',
+                    'Очень хороший ответ!',
+                    'Талантливо!',
+                    'Ты сегодня прыгнул выше головы!',
+                    'Я поражен!',
+                    'Уже существенно лучше!',
+                    'Потрясающе!',
+                    'Замечательно!',
+                    'Прекрасное начало!',
+                    'Так держать!',
+                    'Ты на верном пути!',
+                    'Здорово!',
+                    'Это как раз то, что нужно!',
+                    'Я тобой горжусь!',
+                    'С каждым разом у тебя получается всё лучше!',
+                    'Мы с тобой не зря поработали!',
+                    'Я вижу, как ты стараешься!',
+                    'Ты растешь над собой!',
+                    'Ты многое сделал, я это вижу!',
+                    'Теперь у тебя точно все получится!'
     ]
     Commendation.objects.create(text=random.choice(praises), created = lesson.date,
                                 schoolkid=child, subject=lesson.subject, teacher=lesson.teacher)
-        
+    
